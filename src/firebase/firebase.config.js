7452429e05b625e0593d031d971519f14c70a921
codewithrabby <iammdgolamrabby@gmail.com>
2025-11-11 01:01:30 +0600

    dotenv file added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDdX2Enh3MnyUgkc_jWD5ALcfpRq6uLAE8",
-  authDomain: "food-for-all-f5b23.firebaseapp.com",
-  projectId: "food-for-all-f5b23",
-  storageBucket: "food-for-all-f5b23.firebasestorage.app",
-  messagingSenderId: "36199453599",
-  appId: "1:36199453599:web:2d1cd53a7d00b1930763aa"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
